bfbcd8f1510e1174bb1f6800968379064ca6a64a
Nikhil Garg <nikhil@gargmail.com>
2025-05-28 23:51:36 -0700

    add tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import RootLayoutClient from './RootLayoutClient';
 import "./globals.css";
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: "Options Analyzer",
@@ -14,6 +15,13 @@ export default function RootLayout({
 }>) {
   return (
     <html suppressHydrationWarning lang="en">
+      <head>
+        <Script
+          src="http://stocks.nikhilgarg.com:3000/script.js"
+          data-website-id="6872013d-d46c-4513-80c7-c296a9c6033f"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className="antialiased h-full overflow-auto">
         <RootLayoutClient>
           {children}
